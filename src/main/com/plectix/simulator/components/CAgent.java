7080418a2dea7e23c5eaf673fad42d7079a0952b
Zaal Lianov <zlianov@ggasoftware.com>
2008-09-17 10:48:02 +0000

    added classes for Agent, Site and State

---
+++ src/main/com/plectix/simulator/components/CAgent.java
@@ -0,0 +1,73 @@
+package com.plectix.simulator.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.plectix.simulator.interfaces.IAgent;
+import com.plectix.simulator.interfaces.IInternalState;
+import com.plectix.simulator.interfaces.ILinkState;
+import com.plectix.simulator.interfaces.ISite;
+
+public class CAgent implements IAgent{
+	private String name;
+	private Long dbId;
+	private List<ISite> listSite=new ArrayList<ISite>(1);
+	
+	public CAgent(String name){
+		this.name=name;
+	}
+
+	@Override
+	public void addSite(ISite site){
+		if((site != null) && ( !listSite.contains(site) )){
+			listSite.add(site);
+			((CSite)site).setAgentLink(this);
+		}
+	}
+	
+	@Override
+	public Long getDBId() {
+		return dbId;
+	}
+
+	@Override
+	public List<String> getInterface() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public IInternalState getSiteInternalState(ISite internal_state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ILinkState getSiteLinkState(ISite site) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ISite> getSites() {
+		return listSite;
+	}
+
+	@Override
+	public void setSiteInternalState(ISite site, IInternalState internal_state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSiteLinkState(ISite site, ILinkState link_state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/com/plectix/simulator/components/CSite.java
@@ -0,0 +1,89 @@
+package com.plectix.simulator.components;
+
+import com.plectix.simulator.interfaces.IAgent;
+import com.plectix.simulator.interfaces.IInternalState;
+import com.plectix.simulator.interfaces.ILinkState;
+import com.plectix.simulator.interfaces.ISite;
+import com.plectix.simulator.interfaces.IState;
+
+public class CSite implements ISite {
+	private IState state = null;
+	private String name;
+	private ILinkState linkState;
+	private IInternalState internalState;
+	private boolean changed;
+	private ISite linkSite = null;
+	private IAgent linkAgent = null;
+	private byte statusLink;
+	
+	public static final byte STATUS_LINK_CONNECTED = 0x01;
+	public static final byte STATUS_LINK_MAY_BE = 0x02;
+	public static final byte STATUS_LINK_FREE = 0x04;
+	
+
+	public CSite(String name) {
+		this.name = name;
+		statusLink = STATUS_LINK_FREE;
+	}
+	
+	public void setStatusLink(byte statusLink){
+		this.statusLink=statusLink;
+	}
+	
+	public byte getStatusLink(){
+		return statusLink;
+	}
+	
+
+	public void setAgentLink(IAgent agent){
+		if(agent == null)
+			return;
+		this.linkAgent = agent;
+	}
+	
+	public IAgent getAgentLink(){
+		return linkAgent;
+	}
+	
+	@Override
+	public void setState(IState state) {
+		if (state != null)
+			this.state = state;
+
+	}
+
+	@Override
+	public IInternalState getInternalState() {
+		return internalState;
+	}
+
+	@Override
+	public ILinkState getLinkState() {
+		return linkState;
+	}
+
+	@Override
+	public IState getState() {
+		return state;
+	}
+
+	@Override
+	public boolean isChanged() {
+		return changed;
+	}
+
+	@Override
+	public void setLink(ISite site) {
+		if(site==null)
+			return;
+		linkSite=site;
+		statusLink=STATUS_LINK_CONNECTED;
+//		site.setLink(link);
+	}
+
+	@Override
+	public ISite getLink() {
+		return linkSite;
+	}
+
+}
