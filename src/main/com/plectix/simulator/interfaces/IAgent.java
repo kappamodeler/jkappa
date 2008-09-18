ef0a4daad6ada66d90ad8cd2d588ac57528785fc
J Prudnikova <jprudnikova@ggasoftware.com>
2008-09-18 18:49:19 +0000

    Add some procedures for creating injections

--- src/main/com/plectix/simulator/interfaces/IAgent.java
+++ src/main/com/plectix/simulator/interfaces/IAgent.java
@@ -2,6 +2,7 @@ package com.plectix.simulator.interfaces;
 
 import java.util.List;
 
+import com.plectix.simulator.components.CLinkState;
 import com.plectix.simulator.components.CSite;
 
 
@@ -13,21 +14,21 @@ public interface IAgent {
 	
 	public List<CSite> getSites();  //Returns this Agent�s Sites
 	
-	void addSite(CSite site);
+	public void addSite(CSite site);
 
 	//TODO  it's the set of the agent's sites. maybe we don't need it.
 	public List<String> getInterface();  //Returns this Agent�s Interface which is a list of Strings.
 	
-	public ILinkState getSiteLinkState(ISite site);  //Returns the links state of the given Site. The
+	public CLinkState getSiteLinkState(ISite site);  //Returns the links state of the given Site. The
 										 //Link state can be Free, Bound, or Wildcard.
 	
-	public void setSiteLinkState(ISite site, ILinkState link_state);  //Sets the links state of the given Site to
+	public void setSiteLinkState(ISite site, CLinkState link_state);  //Sets the links state of the given Site to
 												//the given State.
 	
-	public IInternalState getSiteInternalState(ISite internal_state);    //Returns the internal state of the given Site.
+	public String getSiteInternalState(ISite internal_state);    //Returns the internal state of the given Site.
 												//The Internal state can be a String or Wildcard.
 	
-	public void setSiteInternalState(ISite site, IInternalState internal_state);  //Sets the internal state of the given
+	public void setSiteInternalState(ISite site, String internal_state);  //Sets the internal state of the given
 													//Site to the given State.
 
 	
