ad16f1c9025daf898c87f39bfc55b8181862264b
A Lukashin <lukash@ggasoftware.com>
2008-09-18 13:13:28 +0000

    new class SimulatorManager contains methods for building Connected Components and Rules. also it must contain data for simulation

---
+++ src/main/com/plectix/simulator/simulator/SimulatorManager.java
@@ -0,0 +1,85 @@
+package com.plectix.simulator.simulator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.plectix.simulator.components.CAgent;
+import com.plectix.simulator.components.CConnectedComponent;
+import com.plectix.simulator.components.CRule;
+import com.plectix.simulator.components.CSite;
+
+public class SimulatorManager {
+	
+	private static SimulatorManager instance;
+	private List<CRule> rules;
+	
+	private  SimulatorManager() {
+	}
+	
+	public static SimulatorManager getInstance() {
+		if (instance == null) {
+			instance = new SimulatorManager();
+		}
+		return instance;
+	}
+	
+	public List<CConnectedComponent> buildConnectedComponents(List<CAgent> agents) {
+		
+		if(agents == null || agents.isEmpty())
+			return null;
+		
+		List<CConnectedComponent> result = new ArrayList<CConnectedComponent>();
+		
+		while(!agents.isEmpty()) {
+			int index = 0;
+			
+			List<CAgent> connectedAgents = new ArrayList<CAgent>();
+			CAgent agent = agents.remove(0);
+			connectedAgents.add(agent);
+			
+			agent.setDbId(index);
+			
+			List<CSite> sites = agent.getSites();
+			
+			for(CSite site: sites) {
+				if(site.getLinkIndex() != CSite.NO_INDEX) {
+					CAgent linkedAgent = findLink(agents, site.getLinkIndex());
+					if(linkedAgent != null) {
+						connectedAgents.add(linkedAgent);
+						agents.remove(linkedAgent);
+						linkedAgent.setDbId(++index);
+					}
+				}
+			}
+			
+			result.add(new CConnectedComponent(connectedAgents));
+		}
+		
+		return result;
+	}
+	
+	
+
+	private CAgent findLink(List<CAgent> agents, int linkIndex) {
+		for(CAgent tmp: agents) {
+			for (CSite s: tmp.getSites()) {
+				if (s.getLinkIndex() == linkIndex) {
+					return tmp;
+				}
+			}
+		}
+		return null;
+	}
+
+	public CRule buildRule(List<CAgent> left, List<CAgent> right) {
+		return new CRule(buildConnectedComponents(left),buildConnectedComponents(right));
+	}
+
+	public void setRules(List<CRule> rules) {
+		this.rules = rules;
+	}
+
+	public List<CRule> getRules() {
+		return rules;
+	}
+}
