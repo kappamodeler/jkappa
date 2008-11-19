53f71e2732badc112ce4f3b5779ba1ab4c576e7d
A Vokhmin <avokhmin@ggasoftware.com>
2008-11-19 10:47:08 +0000

    fix: ENG-88

--- src/main/com/plectix/simulator/parser/DataReading.java
+++ src/main/com/plectix/simulator/parser/DataReading.java
@@ -31,7 +31,7 @@ public class DataReading {
 
 	private String filePatch = null; // "C:/workspace/Example.tmp";
 
-	private static final String STRING_INITIAL_CONDITIONS_PREFIX = "%init"; // 7; 
+	private static final String STRING_INITIAL_CONDITIONS_PREFIX = "%init"; //7;
 	private static final String STRING_SIMULATION_PREFIX = "%obs"; // 6;
 	private static final String STRING_STORIFY_PREFIX = "%story"; // 8;
 	private static final String STRING_MOD_PREFIX = "%mod"; // 6;
@@ -39,42 +39,43 @@ public class DataReading {
 	public DataReading(String filename) {
 		this.filePatch = filename;
 	}
-	
+
 	private boolean startsWithWhiteSpace(String str) {
 		return str.startsWith(" ") || str.startsWith("\t");
 	}
-	
-	private final String handleModifier(int index, String line, String modifier) 
-					throws ParseErrorException {
-		
+
+	private final String handleModifier(int index, String line, String modifier)
+			throws ParseErrorException {
+
 		String significant = line;
-		
+
 		if (!line.startsWith(modifier)) {
-			throw new ParseErrorException(new CDataString(index, line), 
-					"'" + modifier + "' expected in line : " + line);
+			throw new ParseErrorException(new CDataString(index, line), "'"
+					+ modifier + "' expected in line : " + line);
 		} else {
 			significant = line.substring(modifier.length());
 		}
-		
+
 		boolean separated = false;
 		if (startsWithWhiteSpace(significant)) {
 			significant = significant.trim();
 			separated = true;
 		}
-		
+
 		if (significant.startsWith(":")) {
 			significant = significant.substring(1).trim();
 		} else {
 			if (!separated) {
-				throw new ParseErrorException(new CDataString(index, line), 
-						"'" + modifier + "' expected in line : " + line);
+				throw new ParseErrorException(new CDataString(index, line), "'"
+						+ modifier + "' expected in line : " + line);
 			}
 		}
-		
+
 		return significant;
 	}
 
-	public final void readData() throws FileReadingException, ParseErrorException {
+	public final void readData() throws FileReadingException,
+			ParseErrorException {
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(filePatch));
 			String line;
@@ -85,8 +86,8 @@ public class DataReading {
 				if (line.startsWith("#"))
 					continue;
 				if (line.indexOf("#") != -1)
-					line = line.substring(0, line.indexOf("#"));
-				
+					line = dellComment(line);
+
 				if (line.indexOf("\\") != -1) {
 					String nextLine;
 					nextLine = in.readLine().trim();
@@ -95,16 +96,20 @@ public class DataReading {
 				}
 
 				if (line.startsWith(STRING_MOD_PREFIX)) {
-					String significant = handleModifier(index, line, STRING_MOD_PREFIX);
+					String significant = handleModifier(index, line,
+							STRING_MOD_PREFIX);
 					mods.add(new CDataString(index, significant));
 				} else if (line.startsWith("%story")) {
-					String significant = handleModifier(index, line, STRING_STORIFY_PREFIX);
+					String significant = handleModifier(index, line,
+							STRING_STORIFY_PREFIX);
 					story.add(new CDataString(index, significant));
 				} else if (line.startsWith(STRING_SIMULATION_PREFIX)) {
-					String significant = handleModifier(index, line, STRING_SIMULATION_PREFIX);
+					String significant = handleModifier(index, line,
+							STRING_SIMULATION_PREFIX);
 					observables.add(new CDataString(index, significant));
 				} else if (line.startsWith(STRING_INITIAL_CONDITIONS_PREFIX)) {
-					String significant = handleModifier(index, line, STRING_INITIAL_CONDITIONS_PREFIX);
+					String significant = handleModifier(index, line,
+							STRING_INITIAL_CONDITIONS_PREFIX);
 					inits.add(new CDataString(index, significant));
 				} else if (line.trim().length() > 0) {
 					rules.add(new CDataString(index, new String(line)));
@@ -118,9 +123,40 @@ public class DataReading {
 
 		// checking of the components of data
 		if (rules.isEmpty())
-			throw new FileReadingException("There are no rules in the input data");
+			throw new FileReadingException(
+					"There are no rules in the input data");
 		else if (inits.isEmpty())
-			throw new FileReadingException("There are no inits in the input data (use '%init:')");
+			throw new FileReadingException(
+					"There are no inits in the input data (use '%init:')");
+	}
+
+	private final String dellComment(String line) {
+		String st = new String(line);
+
+		int indexComment = st.lastIndexOf("#");
+		if (indexComment == -1)
+			return line;
+		int indexStart = st.indexOf("'");
+		if (indexStart == -1)
+			line.substring(0, indexComment);
+
+		byte count = 0;
+		int index = 0;
+		for (char ch : line.toCharArray()) {
+			if (ch == "'".hashCode())
+				count++;
+			if (ch == "#".hashCode())
+				if (count != 1) {
+					return line.substring(0,index);
+				}
+			if(count==2)
+				count=0;
+			index++;
+
+		}
+
+		return line;
+
 	}
 
 	public final List<CDataString> getInits() {
