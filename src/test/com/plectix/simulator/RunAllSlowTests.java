9e0446ae54a3fcec91696f2cbea087f2d9317095
Tatyana Lopatkina <lopatkinat@itcwin.com>
2009-04-20 17:29:57 +0000

    ENG-142: RunAllSlowTests class added

---
+++ src/test/com/plectix/simulator/RunAllSlowTests.java
@@ -0,0 +1,14 @@
+package com.plectix.simulator;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import com.plectix.simulator.smiles.RunSmilesTest;
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value = {
+		RunSmilesTest.class
+	})
+public class RunAllSlowTests {
+
+}
