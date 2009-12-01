121560448d3b6fd628463b98f06a88be4e927af9
A Vokhmin <avokhmin@ggasoftware.com>
2009-12-01 13:10:10 +0000

    ENG-345, ENG-164 - added smoke test

--- tests/com/plectix/simulator/smoke/SmokeTestMain.java
+++ tests/com/plectix/simulator/smoke/SmokeTestMain.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value = Suite.class)
 @SuiteClasses(value = { 
 		TestENG229.class,
-		TestENG310.class
+		TestENG310.class,
+		TestENG345.class
 		})
 public class SmokeTestMain {
 
---
+++ tests/com/plectix/simulator/smoke/TestENG345.java
@@ -0,0 +1,78 @@
+package com.plectix.simulator.smoke;
+
+import junit.framework.Assert;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.plectix.simulator.simulator.SimulationData;
+import com.plectix.simulator.simulator.Simulator;
+import com.plectix.simulator.simulator.SimulatorCommandLine;
+import com.plectix.simulator.simulator.SimulationArguments.SimulationType;
+import com.plectix.simulator.util.Info.InfoType;
+
+public class TestENG345 extends SmokeTest {
+	private static final String inputFile = "eng345" + DEFAULT_EXTENSION_FILE;
+
+	@Override
+	protected String[] prepareTestArgs() {
+		String[] args = new String[2];
+
+		args[0] = "--compile";
+		args[1] = inputDirectory + inputFile;
+		return args;
+	}
+
+	@Override
+	public void test() {
+		long timeStamp = System.currentTimeMillis();
+		PropertyConfigurator.configure(LOG4J_PROPERTIES_FILENAME);
+		Simulator mySimulator = new Simulator();
+
+		String[] testArgs = prepareTestArgs();
+
+		SimulationData simulationData = mySimulator.getSimulationData();
+		simulationData.setPrintStream(System.out);
+
+		SimulatorCommandLine commandLine = null;
+		try {
+			commandLine = new SimulatorCommandLine(testArgs);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(e);
+		}
+		simulationData.setSimulationArguments(InfoType.OUTPUT, commandLine
+				.getSimulationArguments());
+		simulationData.readSimulatonFile(InfoType.OUTPUT);
+		simulationData.getKappaSystem().initialize(InfoType.OUTPUT);
+
+		simulationData.setClockStamp(System.currentTimeMillis());
+
+		if (simulationData.getSimulationArguments().isCompile()) {
+			simulationData.outputData();
+			timeStamp  = System.currentTimeMillis() - timeStamp;
+			Assert.assertTrue("Too long", timeStamp < 3000);
+			return;
+		}
+
+		try {
+			if (!simulationData.getSimulationArguments().isDebugInit()) {
+				if (simulationData.getSimulationArguments().isGenereteMap()
+						|| simulationData.getSimulationArguments()
+								.getSimulationType() == SimulationType.CONTACT_MAP) {
+				} else if (simulationData.getSimulationArguments().isStorify()) {
+					mySimulator.runStories();
+				} else {
+					mySimulator.run();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		timeStamp  = System.currentTimeMillis() - timeStamp;
+		
+		Assert.assertTrue("Too long", timeStamp < 3000);
+	}
+
+}
