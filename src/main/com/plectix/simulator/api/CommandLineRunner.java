9b464b7821fb9e0c61b04e37b460297ea69eb565
Ihsan Ecemis <ecemis@alum.bu.edu>
2009-12-07 03:21:54 +0000

    A convenience function to run a simulation passing command line arguments

---
+++ src/main/com/plectix/simulator/api/CommandLineRunner.java
@@ -0,0 +1,22 @@
+package com.plectix.simulator.api;
+
+import org.apache.commons.cli.ParseException;
+
+import com.plectix.simulator.controller.SimulatorCallable;
+import com.plectix.simulator.controller.SimulatorFutureTask;
+import com.plectix.simulator.controller.SimulatorInputData;
+import com.plectix.simulator.simulator.Simulator;
+import com.plectix.simulator.simulator.SimulatorCommandLine;
+
+public class CommandLineRunner {
+	
+	public static final SimulatorFutureTask getSimulatorFutureTask(String[] commandLineArguments) throws ParseException {
+		Simulator simulator = new Simulator();
+		
+		SimulatorCommandLine commandLine = new SimulatorCommandLine(commandLineArguments);
+		SimulatorInputData inputData = new SimulatorInputData(commandLine.getSimulationArguments());
+		SimulatorCallable callable = new SimulatorCallable(simulator, inputData, null);
+		return new SimulatorFutureTask(callable);
+	}
+
+}
